304fde81cb3c54ba3b63ab1bed2bf5f0849d3866
nemuelsousa <nemuel_r@hotmail.com>
2023-04-04 09:42:26 -0300

    feat: add remote backend

--- provider.tf
+++ provider.tf
@@ -1,13 +1,18 @@
 terraform {
   required_providers {
     aws = {
-      source   = "hashicorp/aws"
-      version  = "4.48.0"
-    }   
+      source  = "hashicorp/aws"
+      version = "4.48.0"
+    }
+  }
+  backend "s3" {
+    bucket = "backend-terraform-govbr"
+    key    = "dev/terraform.tfstate"
+    region = "us-east-1"
   }
 }
 
 provider "aws" {
-    region = "us-east-1"
-  
+  region = "us-east-1"
+
 }
