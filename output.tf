e862e1eba65cb63b1945b47f40b6f142b54886c8
nemuelsousa <nemuel_r@hotmail.com>
2023-08-28 10:25:41 -0300

    fix: add output to module

--- output.tf
+++ output.tf
@@ -1,11 +1,11 @@
-# output "oidc" {
-#   value = module.eks_cluster.oidc
-# }
+output "certificate_authority" {
+   value = module.eks_cluster.certificate_authority
+}
 
-# output "ca" {
-#   value = module.eks_cluster.certificate_authority
-# }
+output "endpoint" {
+   value = module.eks_cluster.endpoint
+}
 
-# output "endpoint" {
-#   value = module.eks_cluster.endpoint
-# }
+output "cluster_name" {
+   value = module.eks_cluster.cluster_name
+}
